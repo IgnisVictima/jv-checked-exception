77e7f0618cd0c64e789ef45c2c5ca6a61c5d8ebb
Vitalii Smuraha <smuraga.v@gmail.com>
2026-04-27 08:52:38 +0200

    Var2

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class PasswordValidationException extends Exception {
 
     public PasswordValidationException(String message) {
-        super(message); 
+        super(message);
     }
-    
+
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class UserService {
-    PasswordValidator passwordValidator = new PasswordValidator();
+    public PasswordValidator passwordValidator = new PasswordValidator();
 
     public void registerUser(User user) {
         try {
