c2ccd397589934c66e94edcf1e968e46cbe90f3d
Vitalii Smuraha <smuraga.v@gmail.com>
2026-04-27 08:48:13 +0200

    New variant

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,10 +4,10 @@ public class PasswordValidator {
 
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
          
-        if (password == null || repeatPassword  == null) {
-            throw new PasswordValidationException("Empty fields!");
-        }else if (!(password.equals(repeatPassword) && !(password.length() < 10))) {
-            throw new PasswordValidationException("Wrong passwords");
-        } 
+        if (password == null || repeatPassword == null 
+            || !password.equals(repeatPassword) 
+            || password.length() < 10) {
+                throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
